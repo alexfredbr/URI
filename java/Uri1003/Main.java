e8c78724d85eec62bd3aa12c76357007423b3db3
Alexandre Frederick <37912272+alexfredbr@users.noreply.github.com>
2019-11-26 20:41:25 -0300

    Create Main.java

---
+++ java/Uri1003/Main.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author frede
+ */
+
+public class Main {
+
+    public static void main(String[]  args){
+        
+    Scanner sc = new Scanner(System.in);
+    int inteiro1, inteiro2, SOMA;
+    
+    inteiro1 = sc.nextInt();
+    inteiro2 = sc.nextInt();
+    
+    SOMA = inteiro1 + inteiro2;
+    
+        System.out.println("SOMA = "+SOMA);
+    
+    
+    }
+    
+
+    
+    
+    
+}
